390aeb80428a83d38cad73df51b1baab72668ff9
drew2a <for.drew@gmail.com>
2020-12-17 12:12:39 +0100

    Change walker to EdgeWalk

--- src/tribler-core/tribler_core/modules/popularity/channel_discovery_booster.py
+++ src/tribler-core/tribler_core/modules/popularity/channel_discovery_booster.py
@@ -2,7 +2,7 @@ import logging
 
 from pony.orm import count, db_session
 
-from ipv8.peerdiscovery.discovery import RandomWalk
+from ipv8.peerdiscovery.discovery import EdgeWalk
 from tribler_core.modules.metadata_store.community.remote_query_community import RemoteQueryCommunity
 from tribler_core.modules.metadata_store.serialization import CHANNEL_TORRENT
 
@@ -10,9 +10,7 @@ from tribler_core.modules.metadata_store.serialization import CHANNEL_TORRENT
 class ChannelDiscoveryBoosterMixin(RemoteQueryCommunity):
     TIMEOUT_IN_SEC = 30.0
     MAX_PEERS = 200
-
-    # Stop walking to bootstrap nodes after you have this number of peers.
-    RESET_TO_BOOTSTRAP_THRESHOLD = 20
+    RESULTS_PER_QUERY = 50
 
     TAKE_STEP_INTERVAL_IN_SEC = 0.05
 
@@ -26,16 +24,13 @@ class ChannelDiscoveryBoosterMixin(RemoteQueryCommunity):
         self.saved_max_peers = self.max_peers
         self.max_peers = ChannelDiscoveryBoosterMixin.MAX_PEERS
 
-        self.walker = RandomWalk(self, timeout=ChannelDiscoveryBoosterMixin.TIMEOUT_IN_SEC,
-                                 window_size=ChannelDiscoveryBoosterMixin.MAX_PEERS)
+        # values for neighborhood_size and edge_length were found empirically to
+        # maximize peer count at the end of a 30 seconds period
+        self.walker = EdgeWalk(self, neighborhood_size=25, edge_length=25)
 
         self._registered_tasks_names = []
         self.register_task_until_timeout('take step', self.take_step,
                                          interval=ChannelDiscoveryBoosterMixin.TAKE_STEP_INTERVAL_IN_SEC)
-
-        self._reset_threshold_has_been_exceeded = False
-        self.register_task_until_timeout('check reset threshold', self.check_reset_threshold,
-                                         interval=5)
 
         self._time = 0
         self._introduction_response_count = 0
@@ -53,6 +48,7 @@ class ChannelDiscoveryBoosterMixin(RemoteQueryCommunity):
         with db_session:
             channels_count = count(ts for ts in self.mds.ChannelNode if ts.metadata_type == CHANNEL_TORRENT)
         print(f'{self._time}, {len(self.get_peers())}, {self._introduction_response_count}, {channels_count}')
+
         self._time += 1
 
     def clean(self):
@@ -61,18 +57,6 @@ class ChannelDiscoveryBoosterMixin(RemoteQueryCommunity):
         self.max_peers = self.saved_max_peers
         for registered_task_name in self._registered_tasks_names:
             self.cancel_pending_task(registered_task_name)
-
-    def check_reset_threshold(self):
-        if self._reset_threshold_has_been_exceeded:
-            return
-
-        self.mixin_logger.debug('Check bootstrap threshold')
-
-        # Stop walking to bootstrap nodes after you have N peers.
-        if len(self.get_peers()) > ChannelDiscoveryBoosterMixin.RESET_TO_BOOTSTRAP_THRESHOLD:
-            self.mixin_logger.info('Stop reset to bootstrap')
-            self._reset_threshold_has_been_exceeded = True
-            self.walker.reset_chance = 0
 
     def take_step(self):
         self.mixin_logger.debug('Take step')
@@ -89,5 +73,6 @@ class ChannelDiscoveryBoosterMixin(RemoteQueryCommunity):
         self._introduction_response_count += 1
 
         self.mixin_logger.info('Send remote select')
-        self.send_remote_select(peer=peer, metadata_type=CHANNEL_TORRENT, subscribed=True,
-                                attribute_ranges=(("num_entries", 1, None),), last=1)
+        # self.send_remote_select(peer=peer, metadata_type=CHANNEL_TORRENT,
+        #                         attribute_ranges=(("num_entries", 1, None),),
+        #                         last=ChannelDiscoveryBoosterMixin.RESULTS_PER_QUERY)
